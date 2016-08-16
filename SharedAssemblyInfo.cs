d2754badb98c5e6cec363390fb98a2ef779f53f3
atompacman <jeremiecoul@hotmail.com>
2016-08-15 21:38:27 -0400

    [0.0.0.12] Output test binaries in a separate folder

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -4,5 +4,5 @@
  [assembly: AssemblyCompany("FXGuild")]
  [assembly: AssemblyCopyright("MIT License - Copyright (c) 2016 FXGuild")]
  [assembly: AssemblyProduct("CommonCSharp")]
- [assembly: AssemblyVersion("0.0.0.11")]
+ [assembly: AssemblyVersion("0.0.0.12")]
  [assembly: ComVisible(false)]
