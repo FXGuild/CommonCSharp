114b159a63189cc29912d43be99334b276936818
atompacman <jeremiecoul@hotmail.com>
2016-08-15 21:06:45 -0400

    [0.0.0.9] Share common assembly info between projects

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,8 @@
+ using System.Reflection;
+ using System.Runtime.InteropServices;
+ 
+ [assembly: AssemblyCompany("FXGuild")]
+ [assembly: AssemblyCopyright("MIT License - Copyright (c) 2016 FXGuild")]
+ [assembly: AssemblyProduct("CommonCSharp")]
+ [assembly: AssemblyVersion("0.0.0.9")]
+ [assembly: ComVisible(false)]
