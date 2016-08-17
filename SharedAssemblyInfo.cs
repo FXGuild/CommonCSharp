c18896af409cfae4cee3833019fe8cef3eea01c6
atompacman <jeremiecoul@hotmail.com>
2016-08-16 20:35:12 -0400

    [0.0.1.2] Remove auto-generated classes

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -5,5 +5,5 @@
  [assembly: AssemblyCopyright("MIT License - Copyright (c) 2016 FXGuild")]
  [assembly: AssemblyDescription("Utility classes and functions in C#")]
  [assembly: AssemblyProduct("FXGuild.Common")]
- [assembly: AssemblyVersion("0.0.1.1")]
+ [assembly: AssemblyVersion("0.0.1.2")]
  [assembly: ComVisible(false)]
