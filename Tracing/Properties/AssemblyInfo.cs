1af2abfc67e378a6ffb9cb32f3f305d106c45a6e
atompacman <jeremiecoul@hotmail.com>
2016-09-02 11:52:32 -0400

    [0.0.3.5] Rename project TaskMonitor to Tracing

--- Tracing/Properties/AssemblyInfo.cs
+++ Tracing/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("FXGuild.Common.TaskMonitor")]
+[assembly: AssemblyTitle("FXGuild.Common.Tracing")]
 [assembly: Guid("380072b2-b892-4064-8307-cb6cf0d735d4")]
