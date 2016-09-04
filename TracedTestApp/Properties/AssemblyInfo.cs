01a43a2ec8c41db0d2f8fc0ad65c1c0459010da9
atompacman <jeremiecoul@hotmail.com>
2016-09-04 13:34:55 -0400

    [0.0.3.14] Added empty project TracedTestApp

---
+++ TracedTestApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FXGuild.Common.TracedTestApp")]
+[assembly: Guid("2f8294af-d300-477f-bdea-02f09fe7ca4b")]
